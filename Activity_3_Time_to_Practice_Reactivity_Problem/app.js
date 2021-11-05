9c66f39adb21c20358af2f46b48700e2a02bddb0
HaizaCasapao <casapaohaiza00@gmail.com>
2021-11-04 15:29:45 -1200

    Topic 16 - Added watch property on a data vue object

--- Activity_3_Time_to_Practice_Reactivity_Problem/app.js
+++ Activity_3_Time_to_Practice_Reactivity_Problem/app.js
@@ -1,8 +1,22 @@
 const app = Vue.createApp ({
     data() {
         return {
+            counter:0,
+            timer:5000,
+            value:0,
 
         };
+    },
+
+    watch: {
+        counter(value) {
+            if(value >37) {
+                const that = this;
+                setTimeout(function() {
+                    that.counter = 0;
+                }, 5000)
+            }
+        }
     },
     
 });
