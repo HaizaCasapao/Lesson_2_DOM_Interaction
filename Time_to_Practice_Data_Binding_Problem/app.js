7bfbaacf770d7d4eb7224c3112b83d47ab9a552e
HaizaCasapao <casapaohaiza00@gmail.com>
2021-11-04 12:04:56 -1200

    Topic 9 - Added Time to Practice Data Binding

---
+++ Time_to_Practice_Data_Binding_Problem/app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            yourName:'Haiza T. Casapao',
+            yourAge: 21 + ' yrs old',
+            yourAge5: 21 + 5 + ' after five yrs',
+            url: "http://https://i.pinimg.com/originals/62/08/ec/6208ecbf1d02d01c151c4e3bfe259117.png",
+            name: "Haiza T. Casapao"
+        };
+    },
+    methods: {
+        favNumber() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 0;
+            }   else {
+                return 1;
+            }
+        }
+    }
+});
+
+app.mount('#dataBinding');
