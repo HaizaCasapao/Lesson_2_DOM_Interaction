1c1f9e3e86308d5e8817fde3d90ebba5cc41486e
HaizaCasapao <casapaohaiza00@gmail.com>
2021-11-04 17:09:49 -1200

    Topic 28 - Added Methods Property on data vue object

--- Activity_4_Time_to_Practice_Dynamic_Styling/basics-assignment-4-problem/app.js
+++ Activity_4_Time_to_Practice_Dynamic_Styling/basics-assignment-4-problem/app.js
@@ -1,7 +1,49 @@
 const app = Vue.createApp({
     data() {
         return {
-            
+            user: '',
+            user1:false,
+            user2:false,
+            enable: true,
+            color: '',
+            keycolor: '',
+
         };
     },
+
+    methods: {
+        input(event) {
+            this.user = event.target.value;
+            if(this.user === 'user1') {
+                this.user1 = 'user1';
+            }
+            else if(this.user ==='user2') {
+                this.user2 = 'user2';
+            }
+            else {
+                this.user1 = ''
+                this.user2 = ''
+            }
+        },
+        toggle() {
+            this.enable = !this.enable;
+        },
+        SelectColor(event) {
+            this.color = event.target.value;
+            if(this.color === 'red') {
+                this.selectColor = this.color;
+            }
+            else if(this.color === 'yellow') {
+                this.selectColor = this.color;
+            }
+             if(this.color === 'blue') {
+                this.selectColor = this.color;
+            }
+            else{this.selectColor = '';
+            }
+        }
+    }
+    
 });
+
+app.mount('#styling');
